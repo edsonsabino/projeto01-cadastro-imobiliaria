26e89f64dc2477149e1f0a1914eddd82fd15bcac
edsonsabino <100230731+edsonsabino@users.noreply.github.com>
2022-04-26 13:59:42 -0300

    teste_console

--- popula_bancos.py
+++ popula_bancos.py
@@ -1,26 +1,60 @@
 from modules.InterageDB import interageDB
 import pandas as pd
+import os 
 
-#   instanciar banco
-obj_banco=interageDB("root","#Es181192","localhost","atividade_13")
+##   instanciar banco
+#obj_banco=interageDB("root","#Es181192","localhost","db_imobiliaria")
 
-#   Path csv's (achar alguma forma de referenciar OS, talvez)
-        ## Fazer um for para cada elemento na pasta         
-#   Buscar dados de arquivos # raw data
-df1=pd.read_csv("DADOS_1.csv")
-df2=pd.read_csv("DADOS_2.csv")
+##   Path folder with csv's
 
-#   Tratamento dos dados
+current_folder= os.getcwd()         #   Getting current fonder
+csv_folder='csv_data'               #   Folder with csv files
 
-#   Inserção nos bancos (criar função aqui para não ter que repetir código)
+path_csv_folder=os.path.join(current_folder,csv_folder) #   path to csv folder
 
-for linha in dt_grande.intertuples():
-        
-val_id=linha.id
-val_data=linha.data
-val_valor=linha.valor
-            
-str_atr="id, data, valor"
-str_val=str(val_id)+str(val_data)+str(val_valor)
+csv_names=["inquilinos_mock.csv",
+           "proprietarios_mock.csv",
+           "alugueis_mock.csv",
+           "imoveis_mock.csv"]
 
-obj_banco.insert_one("tbl_resultado",str_atr,str_val)
+##    Extract data with and store in data frame  4 times  
+
+
+try:
+    path=os.path.join(path_csv_folder,csv_names[0]) #   Iquilinos
+    df_inq=pd.read_csv(path, sep=",")
+except Exception as e:
+    print(f"Erro in reading {csv_names[0]}", str(e))
+    
+try:
+    path=os.path.join(path_csv_folder,csv_names[1]) #   Proprietarios
+    df_prop=pd.read_csv(path, sep=",")
+except Exception as e:
+    print(f"Erro in reading {csv_names[1]}", str(e))    
+    
+try:
+    path=os.path.join(path_csv_folder,csv_names[2]) #   Alugueis
+    df_alug=pd.read_csv(path, sep=",")
+except Exception as e:
+    print(f"Erro in reading {csv_names[2]}", str(e))
+
+try:
+    path=os.path.join(path_csv_folder,csv_names[3]) #   Imoveis
+    df_imo=pd.read_csv(path, sep=",")
+except Exception as e:
+    print(f"Erro in reading {csv_names[3]}", str(e))    
+
+##  Correct id_imovel mismatch
+
+try:
+    df_alug['id_imovel']=df_imo['id_imovel']
+except Exception as e:
+    print("Error in correction", str(e))
+
+##  Loading data to database
+
+#   para cada df fazer a sequência de insert into
+#   Dentro de cada df fazer intertuples para inserir no banco
+#       Não está claro como conseguirei a lista de valores para enviar ao insert
+#       Farei com iterrows por enquanto
+
