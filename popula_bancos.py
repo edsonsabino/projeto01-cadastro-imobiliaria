0d6dce719ed950179f9d298a895191ce50ed9a2d
edsonsabino <100230731+edsonsabino@users.noreply.github.com>
2022-04-27 17:09:23 -0300

    Ingestão de dados feita sem erros

--- popula_bancos.py
+++ popula_bancos.py
@@ -14,14 +14,13 @@ current_folder= os.getcwd()         #   Getting current fonder
 csv_folder='csv_data'               #   Folder with csv files
 
 path_csv_folder=os.path.join(current_folder,csv_folder) #   path to csv folder
-
 csv_names=["inquilinos_mock.csv","proprietarios_mock.csv",
            "alugueis_mock.csv","imoveis_mock.csv"]
 
 ##    Extract data with and store in data frame  4 times  
 
-#df_inq=csv_to_df(path_csv_folder,csv_names[0])  #   Iquilinos
-#df_prop=csv_to_df(path_csv_folder,csv_names[1]) #   Proprietarios
+df_inq=csv_to_df(path_csv_folder,csv_names[0])  #   Iquilinos
+df_prop=csv_to_df(path_csv_folder,csv_names[1]) #   Proprietarios
 df_alug=csv_to_df(path_csv_folder,csv_names[2]) #   Alugueis
 df_imo=csv_to_df(path_csv_folder,csv_names[3])  #   Imoveis
 
@@ -30,18 +29,16 @@ df_imo=csv_to_df(path_csv_folder,csv_names[3])  #   Imoveis
 df_alug=replace_duplicates(df_alug,"id_aluguel",1000)
 df_alug=replace_duplicates(df_alug,"id_imovel",100)
 
-##  Forces that all id_imovel apear in df_imo
+##  Force that all id_imovel from df_alug apear in df_imo
 
 df_imo["id_imovel"]=df_alug["id_imovel"]
 
-df_dup=df_imo["id_imovel"].duplicated(keep="first")
-print(df_imo[df_dup])
 ##  Loading data to database
 
-#df_to_database(df_inq,"tbl_inquilinos",df_inq.columns)
-#df_to_database(df_prop,"tbl_proprietarios",df_prop.columns)
-#df_to_database(df_imo,"tbl_imoveis",df_imo.columns)
-#df_to_database(df_alug,"tbl_alugueis",df_alug.columns)
+df_to_database(df_inq,"tbl_inquilinos",df_inq.columns)
+df_to_database(df_prop,"tbl_proprietarios",df_prop.columns)
+df_to_database(df_imo,"tbl_imoveis",df_imo.columns)
+df_to_database(df_alug,"tbl_alugueis",df_alug.columns)
 
 
 
--- sql_scripts/create_table.sql
+++ sql_scripts/create_table.sql
@@ -1,35 +1,35 @@
- create database db_imobiliaria;
+ create database if not exists db_imobiliaria;
  use db_imobiliaria;
 
 -- Creating tables
 
 create table if not exists tbl_inquilinos (
- id int auto_increment primary key,
+ id float auto_increment primary key,
  nome varchar(50), 
  data_nasc date
  );
 create table if not exists tbl_proprietarios (
- id int auto_increment primary key,
+ id float auto_increment primary key,
  nome varchar(50), 
  data_nasc date
  );
 create table if not exists tbl_imoveis(
 
-id_imovel int primary key,
+id_imovel float primary key,
 logradouro varchar(50),
 CEP varchar (8),
 bairro varchar(40),
 cidade varchar(40),
-id_proprietario int,
+id_proprietario float,
 
 constraint PropImovel_fkey foreign key (id_proprietario) 
 	references tbl_proprietarios(id)
  );
  create table if not exists tbl_alugueis(
 
-id_aluguel int primary key,
-id_imovel int,
-id_inquilino int,
+id_aluguel float primary key,
+id_imovel float,
+id_inquilino float,
 
 constraint InquiAluguel_fkey foreign key (id_inquilino) references tbl_inquilinos(id),
 constraint ImovAluguel_fkey foreign key (id_imovel) references tbl_imoveis(id_imovel)
